b357a434750e32e5022391bdb9c7e513cb0c14c9
Greg Chelstowski <dustcloud@hotmail.co.uk>
2023-10-11 23:11:47 +0100

    Got rid of updates, now checking top 500.

--- Dustcloud.HackerNews.Repository/Services/HackerNewsService.cs
+++ Dustcloud.HackerNews.Repository/Services/HackerNewsService.cs
@@ -8,7 +8,6 @@ internal class HackerNewsService : IHackerNewsService
 {
     private const string HackerNewsUri = "https://hacker-news.firebaseio.com/v0/";
     private const string TopStories = "topstories.json";
-    private const string UpdatedStories = "updates.json";
     private const string Item = "item/{0}.json";
 
     private readonly HttpClient _hackerNewsClient;
@@ -17,29 +16,6 @@ internal class HackerNewsService : IHackerNewsService
         _hackerNewsClient = new HttpClient();
         _hackerNewsClient.BaseAddress = new Uri(HackerNewsUri);
     }
-
-    //public async Task<IEnumerable<NewsItem>> GetStoriesAsync(int top)
-    //{
-        
-    //    var builder = new StringBuilder();
-    //    builder.Append("[");
-    //    var counter = 0;
-    //    for(var n = 0; n < 500; n++)
-    //    {
-    //        var itemResponse = await _hackerNewsClient.GetStringAsync(string.Format(Item, stories[n]));
-    //        builder.Append(itemResponse);
-    //        if (n < 499)
-    //        {
-    //            builder.Append(",");
-    //        }
-
-    //    }
-
-    //    builder.Append("]");
-    //    var newsItems = JsonConvert.DeserializeObject<List<NewsItem>>(builder.ToString());
-
-    //    return newsItems.OrderByDescending(s => s.Score).Take(top);
-    //}
 
     public async Task<IEnumerable<int>> GetAllTopStoriesAsync()
     {
@@ -50,15 +26,8 @@ internal class HackerNewsService : IHackerNewsService
 
     }
 
-    public async Task<IEnumerable<int>> GetUpdatedStoriesAsync()
-    {
-        var response = await _hackerNewsClient.GetStringAsync(UpdatedStories);
-        var update = JsonConvert.DeserializeObject<Update>(response);
-        
-        return update?.Items;
-    }
 
-    public async Task<NewsItem> GetNewsItemByIdAsync(int id)
+    private async Task<NewsItem> GetNewsItemByIdAsync(int id)
     {
         var response = await _hackerNewsClient.GetStringAsync(string.Format(Item, id));
         var item = JsonConvert.DeserializeObject<NewsItem>(response);
@@ -83,7 +52,5 @@ internal class HackerNewsService : IHackerNewsService
 public interface IHackerNewsService
 {
     Task<IEnumerable<int>> GetAllTopStoriesAsync();
-    Task<IEnumerable<int>> GetUpdatedStoriesAsync();
-    Task<NewsItem> GetNewsItemByIdAsync(int id);
     Task<IEnumerable<NewsItem>> GetNewsItemsByIds(IEnumerable<int> ids);
 }
--- Dustcloud.HackerNews/Controllers/HackerNewsController.cs
+++ Dustcloud.HackerNews/Controllers/HackerNewsController.cs
@@ -13,7 +13,6 @@ public class HackerNewsController : ControllerBase
     private readonly IMemoryCache _memoryCache;
     private readonly ILogger<HackerNewsController> _logger;
     private const string NewsStoryCacheKey = "NewsStory";
-    private const string UpdatedCacheKey = "Updated";
 
     public HackerNewsController(IHackerNewsService hackerNewsService,
         IMemoryCache memoryCache,
@@ -29,36 +28,15 @@ public class HackerNewsController : ControllerBase
     {
         try
         {
-            if (_memoryCache.TryGetValue(NewsStoryCacheKey, out IEnumerable<NewsItem> items))
-            {
-                var topItems = items.OrderByDescending(s => s.Score).Take(top);
-
-                var isUpdatedCached = _memoryCache.TryGetValue(UpdatedCacheKey, out IEnumerable<NewsItem> updatedItems);
-                if (!isUpdatedCached)
-                {
-                    await SetUpdatedItemsFromCacheAsync();
-                }
-                else
-                {
-                    var isUpdatedCacheAndApiSame = await CompareCacheWithApiAsync(updatedItems.ToList());
-                    if (isUpdatedCacheAndApiSame)
-                    {
-                        return Ok(topItems);
-                    }
-                    else
-                    {
-                        await SetUpdatedItemsFromCacheAsync();
-                    }
-                }
-
-            }
-            else
+            if (!_memoryCache.TryGetValue(NewsStoryCacheKey, out List<NewsItem> items) ||
+                !await CompareCacheWithApiAsync(items.OrderByDescending(s => s.Score).Take(top)))
             {
                 await SetTopItemsCacheAsync();
                 _memoryCache.TryGetValue(NewsStoryCacheKey, out items);
             }
-
-            return Ok(items.OrderByDescending(s => s.Score).Take(top));
+            
+            var topItems = items.OrderByDescending(s => s.Score).Take(top);
+            return Ok(topItems);
         }
         catch (Exception ex)
         {
@@ -67,11 +45,12 @@ public class HackerNewsController : ControllerBase
     }
 
     //True if they're the same
-    private async Task<bool> CompareCacheWithApiAsync(IEnumerable<NewsItem> cachedUpdatedItems)
+    private async Task<bool> CompareCacheWithApiAsync(IEnumerable<NewsItem> cachedItems)
     {
-        var updatedIds = (await _hackerNewsService.GetUpdatedStoriesAsync()).ToList();
-        var cachedUpdatedIds = cachedUpdatedItems.Select(s => s.Id).ToList();
-        return updatedIds.Count() == cachedUpdatedIds.Count() && !updatedIds.Except(cachedUpdatedIds).Any();
+        var topStoriesIds = (await _hackerNewsService.GetAllTopStoriesAsync()).ToList();
+        var cachedItemIds = cachedItems.Select(s => s.Id).ToList();
+        return topStoriesIds.FindAll(s => cachedItemIds.Contains(s)).Count == cachedItemIds.Count;
+
     }
 
 
@@ -83,18 +62,10 @@ public class HackerNewsController : ControllerBase
         // This is the long-un
         var storiesWithBodies = await _hackerNewsService.GetNewsItemsByIds(allStories);
 
-        SetCache(NewsStoryCacheKey, storiesWithBodies);
+        SetCache(NewsStoryCacheKey, storiesWithBodies.ToList());
 
     }
 
-    private async Task SetUpdatedItemsFromCacheAsync()
-    {
-        _logger.Log(LogLevel.Information, "Setting updated items cache");
-        var updatedIds = await _hackerNewsService.GetUpdatedStoriesAsync();
-        var updatedItems = await _hackerNewsService.GetNewsItemsByIds(updatedIds);
-
-        SetCache(UpdatedCacheKey, updatedItems, 60);
-    }
 
     private void SetCache(string cacheKey, object body, int slidingExpiration = 300)
     {
